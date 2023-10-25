9433d44ce6c4e2bf0baf87707a754446f5246219
kiang <kiange@gmail.com>
2023-10-25 19:35:26 +0800

    set values using Livewire function

--- resources/views/filament/forms/components/latlng.blade.php
+++ resources/views/filament/forms/components/latlng.blade.php
@@ -2,11 +2,11 @@
     <input type="text" value="" id="latlngHelper" />
 </div>
 <script>
-    document.querySelector('#latlngHelper').addEventListener('change', function() {
+    document.querySelector('#latlngHelper').addEventListener('change', function () {
         let latlng = document.querySelector('#latlngHelper').value.split(',');
-        if(latlng[1]) {
-            document.querySelector('#data\\.latitude').value = parseFloat(latlng[0]);
-            document.querySelector('#data\\.longitude').value = parseFloat(latlng[1]);
+        if (latlng[1]) {
+            @this.set('data.latitude', parseFloat(latlng[0]));
+            @this.set('data.longitude', parseFloat(latlng[1]));
         }
     });
 </script>
